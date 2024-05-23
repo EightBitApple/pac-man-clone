a12aff5d3a787bcc55f8b2644b93d778fcfe51c2
The8BitApple <stefanharris16@gmail.com>
2024-05-23 11:48:43 +0100

    WIP: adding sources.

---
+++ doc/working/bib/.auctex-auto/sources.el
@@ -0,0 +1,22 @@
+(TeX-add-style-hook
+ "sources"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "pygameAbout"
+    "SDLIntro"
+    "kidsCanCodeLessions"
+    "stackoverflowSmoothTileMovement"
+    "gamesAtAGlance"
+    "pacmanDoodle"
+    "20x20Tileset"
+    "kidsCanCodeTileBasedGameP2"
+    "fileEncryptAndDecrypt"
+    "kidsCanCodePlatformerP7Github"
+    "kidsCanCodePlatformerP7Vidro"
+    "pythonTutorialPrivateAttributes"
+    "stackExchangeCollisionBox"
+    "kidsCanCodePlatformerP18Github"
+    "tiledLevelEditor"
+    "pyTMX"))
+ '(or :bibtex :latex))
+
